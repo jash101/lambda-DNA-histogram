007860f0c6b9cb1503596fa25a253164603ea7d4
Jash Mota <jashmota101@gmail.com>
2020-12-07 16:12:16 +0530

    Create findSequence.py

---
+++ findSequence.py
@@ -0,0 +1,38 @@
+from matplotlib import pyplot as plt 
+import numpy as np 
+
+with open('DNAinput.txt', 'r') as file:
+    dnaSequence = file.read().replace('\n', '')
+
+searchSequence = "GG"
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+print(len(dnaSequence))
+dnaSequence = dnaSequence.replace("\n", "")
+print(len(dnaSequence))
+res = [translate(i, 0, len(dnaSequence), 0, 5.94) for i in range(len(dnaSequence)) if dnaSequence.startswith(searchSequence, i)]
+print(str(res))
+
+
+print(translate(30000, 0, len(dnaSequence), 0, 5.94))    
+
+resnp = np.asarray(res)
+  
+# Creating histogram 
+fig, ax = plt.subplots(figsize =(10, 7)) 
+nbins = 600
+ax.hist(resnp, bins = nbins) 
+# Show plot 
+plt.show()
+
+#TODO output txt file of occurrences
